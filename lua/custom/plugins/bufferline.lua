5eacb80adfcc686d5373201a28b002d53aa69b1c
Mond45 <mondc45@gmail.com>
2025-02-23 16:59:34 +0700

    bufferline uses mini.bufremove

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -7,5 +7,9 @@ return {
     { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
     { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
   },
-  opts = {},
+  opts = {
+    options = {
+      close_command = require('mini.bufremove').delete,
+    },
+  },
 }

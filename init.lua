3c992160bba39846457f7051ffbd618d978daa01
Mond45 <mondc45@gmail.com>
2025-02-26 19:53:53 +0700

    nvim-surround instead of mini.surround

--- init.lua
+++ init.lua
@@ -969,6 +969,13 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    opts = {},
+  },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
@@ -985,17 +992,17 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup {
-        mappings = {
-          add = 'gsa', -- Add surrounding in Normal and Visual modes
-          delete = 'gsd', -- Delete surrounding
-          find = 'gsf', -- Find surrounding (to the right)
-          find_left = 'gsF', -- Find surrounding (to the left)
-          highlight = 'gsh', -- Highlight surrounding
-          replace = 'gsr', -- Replace surrounding
-          update_n_lines = 'gsn', -- Update `n_lines`
-        },
-      }
+      -- require('mini.surround').setup {
+      --   mappings = {
+      --     add = 'gsa', -- Add surrounding in Normal and Visual modes
+      --     delete = 'gsd', -- Delete surrounding
+      --     find = 'gsf', -- Find surrounding (to the right)
+      --     find_left = 'gsF', -- Find surrounding (to the left)
+      --     highlight = 'gsh', -- Highlight surrounding
+      --     replace = 'gsc', -- Replace surrounding
+      --     update_n_lines = 'gsn', -- Update `n_lines`
+      --   },
+      -- }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,

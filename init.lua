19c02242e2506e1264a929da8234f5566eae34a2
Mond45 <mondc45@gmail.com>
2025-02-26 19:54:13 +0700

    nvim-ufo

--- init.lua
+++ init.lua
@@ -462,6 +462,26 @@ require('lazy').setup({
       },
     },
   },
+
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = {
+      { 'kevinhwang91/promise-async' },
+      'neovim/nvim-lspconfig',
+    },
+    init = function()
+      vim.o.foldcolumn = '0' -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+    end,
+    config = function()
+      require('ufo').setup()
+
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+    end,
+  },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -648,6 +668,10 @@ require('lazy').setup({
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.

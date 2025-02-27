5696224db9e248c2c9b027266101fa639fe23d40
Mond45 <mondc45@gmail.com>
2025-02-27 15:44:50 +0700

    latex

--- init.lua
+++ init.lua
@@ -759,6 +759,8 @@ require('lazy').setup({
         },
 
         terraformls = {},
+
+        texlab = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -1073,11 +1075,14 @@ require('lazy').setup({
         'cpp',
         'terraform',
         'hcl',
+        'bibtex',
+        'latex',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'latex' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.

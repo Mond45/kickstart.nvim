e0102030a4331b591802e5ef5c11ab0436a3a353
Mond45 <mondc45@gmail.com>
2025-03-18 23:14:29 +0700

    stickybuf auto pin Outline buffers

--- lua/custom/plugins/stickybuf.lua
+++ lua/custom/plugins/stickybuf.lua
@@ -1,4 +1,15 @@
 return {
   'stevearc/stickybuf.nvim',
-  opts = {},
+  opts = {
+    get_auto_pin = function(bufnr)
+      -- You can return "bufnr", "buftype", "filetype", or a custom function to set how the window will be pinned.
+      -- You can instead return an table that will be passed in as "opts" to `stickybuf.pin`.
+      -- The function below encompasses the default logic. Inspect the source to see what it does.
+      local filetype = vim.bo[bufnr].filetype
+      if filetype == 'Outline' then
+        return 'filetype'
+      end
+      return require('stickybuf').should_auto_pin(bufnr)
+    end,
+  },
 }

a86efe3d2278a5665a1082b6b2845131b4a99996
Mond45 <mondc45@gmail.com>
2025-06-25 14:50:15 +0700

    update outline.nvim icons

--- lua/custom/plugins/outline.lua
+++ lua/custom/plugins/outline.lua
@@ -1,7 +1,69 @@
 return {
   {
     'hedyhli/outline.nvim',
-    opts = {},
+    opts = function()
+      local defaults = require('outline.config').defaults
+      local opts = {
+        symbols = {
+          icons = {},
+        },
+        keymaps = {
+          up_and_jump = '<up>',
+          down_and_jump = '<down>',
+        },
+      }
+
+      local icons = {
+        Array = ' ',
+        Boolean = '󰨙 ',
+        Class = ' ',
+        Codeium = '󰘦 ',
+        Color = ' ',
+        Control = ' ',
+        Collapsed = ' ',
+        Constant = '󰏿 ',
+        Constructor = ' ',
+        Copilot = ' ',
+        Enum = ' ',
+        EnumMember = ' ',
+        Event = ' ',
+        Field = ' ',
+        File = ' ',
+        Folder = ' ',
+        Function = '󰊕 ',
+        Interface = ' ',
+        Key = ' ',
+        Keyword = ' ',
+        Method = '󰊕 ',
+        Module = ' ',
+        Namespace = '󰦮 ',
+        Null = ' ',
+        Number = '󰎠 ',
+        Object = ' ',
+        Operator = ' ',
+        Package = ' ',
+        Property = ' ',
+        Reference = ' ',
+        Snippet = '󱄽 ',
+        String = ' ',
+        Struct = '󰆼 ',
+        Supermaven = ' ',
+        TabNine = '󰏚 ',
+        Text = ' ',
+        TypeParameter = ' ',
+        Unit = ' ',
+        Value = ' ',
+        Variable = '󰀫 ',
+      }
+
+      for kind, symbol in pairs(defaults.symbols.icons) do
+        opts.symbols.icons[kind] = {
+          icon = icons[kind] or symbol.icon,
+          hl = symbol.hl,
+        }
+      end
+      return opts
+    end,
     keys = {
       {
         '<leader>o',
